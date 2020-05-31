1c4f6470d22579d2cc53bca1fea5ee21baa6e276
Paul Hoover <polyphonic13@gmail.com>
2020-05-31 14:29:50 -0400

    commented-out mongo stuff in app.js

--- app.js
+++ app.js
@@ -51,16 +51,16 @@ passport.deserializeUser(Account.deserializeUser());
 // mongoose.connect("localhost:27017/polyworksio");
 console.log("starting up node app, db = " + config.db);
 
-mongoose.connect(config.db);
-mongoose.connection.once('open', function() {
-	console.log('mongoose connection open');
-});
-mongoose.connection.on('error', function(err) {
-	console.log('mongoose conection err: ', err);
-});
+// mongoose.connect(config.db);
+// mongoose.connection.once('open', function() {
+// 	console.log('mongoose connection open');
+// });
+// mongoose.connection.on('error', function(err) {
+// 	console.log('mongoose conection err: ', err);
+// });
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
@@ -71,7 +71,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
+    app.use(function (err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
@@ -82,7 +82,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
